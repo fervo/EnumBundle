d69ec947de843f3872cb4469bd114e3a2d56242a
Magnus Nordlander <magnus@nordlander.se>
2016-01-26 14:58:07 +0100

    Added paramconverter and upped minor version.

---
+++ ParamConverter/EnumParamConverter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Fervo\EnumBundle\ParamConverter;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use MyCLabs\Enum\Enum;
+
+/**
+* 
+*/
+class EnumParamConverter implements ParamConverterInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @throws NotFoundHttpException When invalid date given
+     */
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+        $param = $configuration->getName();
+
+        if (!$request->attributes->has($param)) {
+            return false;
+        }
+
+        $options = $configuration->getOptions();
+        $value = $request->attributes->get($param);
+
+        if (!$value && $configuration->isOptional()) {
+            return false;
+        }
+
+        $enumClass = $configuration->getClass();
+
+        if (!call_user_func([$enumClass, 'isValid'], $value)) {
+            throw new NotFoundHttpException('Invalid enum value.');
+        }
+
+        $enum = new $enumClass($value);
+
+        $request->attributes->set($param, $enum);
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(ParamConverter $configuration)
+    {
+        if (null === $configuration->getClass()) {
+            return false;
+        }
+
+        return is_a($configuration->getClass(), Enum::class, true);
+    }
+}
