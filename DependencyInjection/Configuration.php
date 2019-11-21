703968c99c0430d33fbdf9e868eb4dc2df48a166
Marcel Marnitz <marcelmarnitz@googlemail.com>
2019-11-21 21:09:37 +0100

    Drops Symfony 2.8 support and fixes deprecation messages starting from SF4.3+

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fervo_enum');
+        $treeBuilder = new TreeBuilder('fervo_enum');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('fervo_enum');
+        }
 
         $rootNode
             ->children()
